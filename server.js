999eb2a18b79bb929095ee9b48a77f45f4120f80
Dylan Wilkinson <dylanwilkinson@gmail.com>
2017-10-20 21:01:42 -0700

    handlebars template

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const request = require('request');
 const async = require('async');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs'  }));
+app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use (morgan('dev'));
+
+app.get('/', (req, res, next) => {
+    res.render('main/home');
+});
+
+
 
 app.listen(3030, (err) => {
     if (err) {
